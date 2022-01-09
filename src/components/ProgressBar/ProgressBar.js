1b7671ad672e7886b3523425bd8b202d96a2ee44
Le Quang Thang <lethang7794@gmail.com>
2022-01-09 16:27:53 +0700

    Refactor to use CSS variable and some small fixes
    - Add text for accessibility.
    - Fix active bar tail not has radius if nearly full.

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -5,43 +5,71 @@ import styled from 'styled-components';
 import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
-const styles = {
-  large: { height: '24px', padding: '4px' },
-  medium: { height: '12px' },
-  small: { height: '8px' },
+const STYLES = {
+  large: {
+    height: 16,
+    padding: 4,
+    radius: 8,
+  },
+  medium: {
+    height: 12,
+    padding: 0,
+    radius: 4,
+  },
+  small: {
+    height: 8,
+    padding: 0,
+    radius: 4,
+  },
 };
 
 const Wrapper = styled.div`
-  width: 370px;
-  overflow: hidden;
   background-color: ${COLORS.transparentGray15};
-  box-shadow: inset 0px 2px 4px rgba(128, 128, 128, 0.35);
-  border-radius: 8px;
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+  padding: var(--padding);
+  border-radius: var(--radius);
 `;
 
 const RadiusOverflow = styled.div`
-  height: 100%;
   border-radius: 4px;
+
+  /* Trim off progress bar's head and tail (if it nearly full) */
   overflow: hidden;
 `;
 
 const ActiveBar = styled.div`
   background-color: ${COLORS.primary};
-  width: ${({ value }) => value}%;
-  height: 100%;
+  width: var(--width);
+  height: var(--height);
 `;
 
 const ProgressBar = ({ value, size }) => {
+  const style = STYLES[size];
+  if (!style) {
+    throw new Error(`Unknown size passed to ProgressBar: ${size} `);
+  }
+
   return (
     <Wrapper
       role='progressbar'
+      aria-valuenow={value}
       aria-valuemin='0'
       aria-valuemax='100'
-      aria-valuenow={value}
-      style={styles[size]}
+      style={{
+        '--padding': style.padding + 'px',
+        '--radius': style.radius + 'px',
+      }}
     >
       <RadiusOverflow>
-        <ActiveBar value={value} size={size} />
+        <ActiveBar
+          value={value}
+          size={size}
+          style={{
+            '--width': value + '%',
+            '--height': style.height + 'px',
+          }}
+        />
+        <VisuallyHidden>{value}%</VisuallyHidden>
       </RadiusOverflow>
     </Wrapper>
   );
