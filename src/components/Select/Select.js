5123f773ddbad1276e86256c5ac1c76f251947d1
Le Quang Thang <lethang7794@gmail.com>
2022-01-09 19:09:21 +0700

    Refactor Select component
    - Remove unnecessary useRef.
    - Center the icon in a better manner.
    ...

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 
 import { COLORS } from '../../constants';
@@ -8,68 +8,24 @@ import { getDisplayedValue } from './Select.helpers';
 const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
-  const selectRef = useRef();
-
-  const handleFocusSelect = () => {
-    if (selectRef.current) {
-      selectRef.current.focus();
-    }
-  };
-
   return (
-    <Wrapper onClick={handleFocusSelect}>
-      <DisplayValue>{displayedValue}</DisplayValue>
-      <IconWrapper>
-        <Icon id='chevron-down' as='span' size={24} />
-      </IconWrapper>
+    <Wrapper>
       <NativeSelect label={label} value={value} onChange={onChange}>
         {children}
       </NativeSelect>
+      <SelectPresentation>
+        <DisplayValue>{displayedValue}</DisplayValue>
+        <IconWrapper style={{ '--size': 24 + 'px' }}>
+          <Icon id='chevron-down' as='span' size={24} />
+        </IconWrapper>
+      </SelectPresentation>
     </Wrapper>
   );
 };
 
 const Wrapper = styled.div`
   position: relative;
-
-  display: inline-block;
-  width: fit-content;
-
-  background-color: ${COLORS.transparentGray15};
-  border-radius: 8px;
-  border: none;
-
-  &:focus-within {
-    outline: 2px solid #4374cb;
-  }
-`;
-
-const DisplayValue = styled.span`
-  display: inline-block;
-
-  font-family: 'Roboto', sans-serif;
-  font-weight: 400;
-  font-size: 16px;
-  line-height: 19px;
-
-  color: ${COLORS.gray700};
-  padding: 12px 52px 12px 16px;
-
-  ${Wrapper}:hover & {
-    color: ${COLORS.black};
-  }
-`;
-
-const IconWrapper = styled.span`
-  display: inline-block;
-  position: absolute;
-  top: 10px;
-  right: 10px;
-
-  color: ${COLORS.gray700};
-  ${Wrapper}:hover & {
-    color: ${COLORS.black};
-  }
+  width: max-content;
 `;
 
 const NativeSelect = styled.select`
@@ -80,6 +36,46 @@ const NativeSelect = styled.select`
   height: 100%;
 
   opacity: 0;
+`;
+
+const SelectPresentation = styled.div`
+  background-color: ${COLORS.transparentGray15};
+  border-radius: 8px;
+  border: none;
+
+  color: ${COLORS.gray700};
+
+  ${NativeSelect}:focus + & {
+    outline: 1px dotted #212121;
+    outline: 5px auto -webkit-focus-ring-color;
+  }
+
+  ${NativeSelect}:hover + & {
+    color: ${COLORS.black};
+  }
+`;
+
+const DisplayValue = styled.div`
+  font-family: 'Roboto', sans-serif;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 19px;
+
+  padding: 12px 16px;
+  padding-right: 52px;
+`;
+
+const IconWrapper = styled.span`
+  position: absolute;
+  top: 0;
+  right: 10px;
+  bottom: 0px;
+
+  width: var(--size);
+  height: var(--size);
+  margin: auto;
+
+  pointer-events: none;
 `;
 
 export default Select;
